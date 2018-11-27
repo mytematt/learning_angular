400c10ce903bfcfd4503af7e0d23e1140d80e207
Matt Wilkinson <mytematt@gmail.com>
2018-11-27 09:55:05 -0700

    thru lecture 14

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent { }
+export class UserComponent { 
+  firstName = 'John';
+  lastName = 'Smith';
+  age = 30;
+  address = {
+    street: '50 Main St',
+    city: 'Boston',
+    state: 'MA'
+  }
+
+  constructor() {
+    
+  }
+
+  showAge() {
+    return this.age + 2
+  }
+}
