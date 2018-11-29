b9effcc419b70ce9418f1d796910d850bdeb09bb
Matt Wilkinson <mytematt@gmail.com>
2018-11-29 12:22:27 -0700

    thru lecture 16

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,50 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent { 
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
+export class UserComponent implements OnInit { 
+  user: User;
 
-  foo: any;
-  hasKids: boolean;
-  numberArray: number[];
-  stringArray: string[];
-  mixedArray: any[];
-  myTuple: [string, number, boolean];
-  unusable: void;
-  u: undefined;
-  n: null;
 
   constructor() {
-    this.firstName = 'John';
-    this.lastName = 'Doe';
-    this.age = 30;
-    this.address = {
-      street: '50 Main St',
-      city: 'Boston',
-      state: 'MA'
-    }
-    this.hasKids = true;
-    this.numberArray = [1,2,3]
-
-    console.log(this.addNumbers(2,3))
+  }
   
-  }
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main St',
+        city: 'Boston',
+        state: 'MA'
+      }
+    }
 
-  showAge() {
-    return this.age + 2
   }
-
-  addNumbers(num1: number, num2: number): number {
-    return num1 + num2
-  }
-
 
 
 }
+
+
