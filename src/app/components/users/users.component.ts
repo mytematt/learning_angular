db38c972216d0515b11d3bc0247a4ea1a16277b7
Matt Wilkinson <mytematt@gmail.com>
2018-12-07 12:50:40 -0700

    through lecture 20

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User'; 
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+  enableAdd: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+   
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA'
+          },
+          image: 'https://picsum.photos/600/600?image=1027'
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 34,
+          address: {
+            street: '20 School st',
+            city: 'Lynn',
+            state: 'MA'
+          },
+          image: 'https://picsum.photos/600/600?image=1024'
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 26,
+          address: {
+            street: '55 Mill st',
+            city: 'Miami',
+            state: 'FL'
+          },
+          image: 'https://picsum.photos/600/600?image=1011'
+        }
+      ];
+
+      this.loaded = true;
+
+      // this.addUser({
+      //   firstName: 'David',
+      //   lastName: 'Jackson'
+      // });
   }
 
+  addUser(user: User) {
+    this.users.push(user);
+  }
+  
 }
